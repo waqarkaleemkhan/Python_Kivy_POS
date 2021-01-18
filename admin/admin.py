f4b273e07990e8d06ecbe586d4cf7e4a76f214c5
waqarkaleemkhan <waqar.musa777@gmail.com>
2021-01-18 17:38:20 +0500

    buttons added to admin page and the admin page is connected to database

--- admin/admin.py
+++ admin/admin.py
@@ -1,10 +1,21 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
-
+import psycopg2
 
 class AdminWindow(BoxLayout):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
+
+
+    conn=psycopg2.connect(
+        host="localhost",
+        database="swat_cash_and_carry",
+        user="postgres",
+        password="root"
+    )
+
+
+    conn.close()
 class AdminApp(App):
     def build(self):
         return AdminWindow()
